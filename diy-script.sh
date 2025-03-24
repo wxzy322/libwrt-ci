d512a694d90e4a80e2c5ff08235e74ed580f254f
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2025-03-24 14:52:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -5,6 +5,10 @@ sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
 
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
+
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
