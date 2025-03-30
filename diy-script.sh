44c989bb4d6c0cfd8f81cca0325e4cecc07c8bd9
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2025-03-30 17:28:03 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
 
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+# sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
 
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
